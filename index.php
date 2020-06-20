3839638243f8c690df02964f13d4babf547c2129
Habib Aroua <habib.aroua@sesame.com.tn>
2020-06-21 00:27:16 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+    // The data to send to the API
+    $authToken = 'OAuth 2.0 token here';
+    if((isset($_POST['user'])) && (isset($_POST['password'])))
+    {
+        $user = $_POST['user'];
+        $password = $_POST['password'];
+        $postData = array
+        (
+            'jsonrpc' => '2.0',
+            'method' => 'user.login',
+            'params' => array('user' => $user , 'password' => $password),
+            'id' => 1,
+            'auth' => null
+        );
+
+        // Setup cURL
+        $ch = curl_init('http://51.178.169.200/zabbix/api_jsonrpc.php');
+        curl_setopt_array
+        (
+            $ch, array
+            (
+                CURLOPT_POST => TRUE,
+                CURLOPT_RETURNTRANSFER => TRUE,
+                CURLOPT_HTTPHEADER => array
+                (
+                    'Authorization: '.$authToken,
+                    'Content-Type: application/json'
+                ),
+                CURLOPT_POSTFIELDS => json_encode($postData)
+            )
+        );
+        // Send the request
+        $response = curl_exec($ch);
+
+        // Check for errors
+        if($response === FALSE)
+        {
+            die(curl_error($ch));
+            echo "Error";
+        }
+       
+        // Decode the response
+        $responseData = json_decode($response, TRUE);
+        echo $response;
+    }
+    else
+    {
+        echo "Error";
+    }
+?>
