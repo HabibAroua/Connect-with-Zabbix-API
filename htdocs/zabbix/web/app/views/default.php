36a75eacafec91fa860482d7e4481b8b5e81e50f
Habib Aroua <HabibAroua@users.noreply.github.com>
2020-08-10 22:54:08 +0100

    Create default.php

---
+++ htdocs/zabbix/web/app/views/default.php
@@ -0,0 +1,20 @@
+<div id="page-inner">
+    <div class="row">
+        <div class="col-md-12">
+            <center>
+                <h2>News</h2>
+            </center>
+        </div>
+		<div class="row">
+            <div class="col-md-12">
+				<div class="panel panel-default">
+					<div class="panel-body">
+                        <div class="row">
+							
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
