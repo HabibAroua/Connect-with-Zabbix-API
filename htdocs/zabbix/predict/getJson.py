38686be1e0596af382b92aae811349d066853419
Habib Aroua <HabibAroua@users.noreply.github.com>
2020-08-15 23:54:28 +0100

    Update getJson.py

--- htdocs/zabbix/predict/getJson.py
+++ htdocs/zabbix/predict/getJson.py
@@ -2,6 +2,8 @@ import urllib.request, json
 with urllib.request.urlopen("http://localhost/zabbix/json/getAllSlaByService.php") as url:
     data = json.loads(url.read().decode())
     print(len(data))
-
+tab =[]
 for i in range(0,len(data)):
-	print(data[i]['host_name'] )
+	tab.append(data[i]['host_name'])
+	
+print(tab)
