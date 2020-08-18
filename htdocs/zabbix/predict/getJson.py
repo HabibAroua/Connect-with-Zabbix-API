099da67cc761e8accff9ca0c35464febb20899e0
Habib Aroua <HabibAroua@users.noreply.github.com>
2020-08-18 19:23:53 +0100

    add all method which can manage an extra file

--- htdocs/zabbix/predict/getJson.py
+++ htdocs/zabbix/predict/getJson.py
@@ -1,8 +1,38 @@
 import urllib.request, json
+import urllib.parse
+import re
 import sys
 sys.path.append('element.py')
 from element import Element
 
+def readFile(fileName):
+    try:
+        with open(fileName, 'r') as file:
+            data = file.read().replace('\n', '')
+        return data
+    except Exception as e : 
+        print(str(e))
+
+def writeFile(fileName,line):
+    try:
+        with open(fileName,'w') as f:
+            f.write(line)
+    except Exception as e : 
+        print("This file is not found : "+str(e))
+        
+def addNewLineInFile(fileName,line):
+    try:
+        hs = open(fileName,"a")
+        hs.write(line+"\n")
+        hs.close()
+    except Exception as e : 
+        print(str(e))
+        
+def createNewFile(fileName):
+    try:
+        f= open(fileName,"w+")
+    except Exception as e : 
+        print(str(e))
 
 with urllib.request.urlopen("http://localhost/zabbix/json/getAllSlaByService.php") as url:
     data = json.loads(url.read().decode())
@@ -13,6 +43,7 @@ for i in range(0,len(data)):
     e=Element(data[i]['host_name'],data[i]['list'])
     tab.append(e)
 
+createNewFile('result.py')
 
 for i in range(0 , len(tab)):
     #print (tab[i].getHost_name())
