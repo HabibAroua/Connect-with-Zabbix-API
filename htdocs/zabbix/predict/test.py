ec52b1576621a36447a290c97b9c3c273d1a5c76
Habib Aroua <HabibAroua@users.noreply.github.com>
2020-09-02 11:43:52 +0100

    Create test.py

---
+++ htdocs/zabbix/predict/test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+import sys
+
+print ('Number of arguments:', len(sys.argv), 'arguments.')
+print ('Argument List:', str(sys.argv))
+print (sys.argv[1])
+print ("habib".lower() == str(sys.argv[1]).lower())
