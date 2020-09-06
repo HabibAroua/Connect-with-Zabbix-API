1f7e921c62fed16058f3af985fe01ec677bae3d5
Habib Aroua <HabibAroua@users.noreply.github.com>
2020-09-06 12:32:47 +0100

    Update Register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,162 +1,43 @@
-import React , {Component} from 'react'
-import axios from "axios";
 import './Login.css';
+import React from 'react';
+import axios from 'axios';
 
-class Register extends Component
-{
-    insert(user)
-    {
-		
-	    return axios
-            .post('http://127.0.0.1/zabbix/json/get_nb_value_sla.php',
-			{
-                value : user.email
-			})
-			
-            .then(res =>
-            {
-				alert(res.data);
-				if(res.data== "Error")
-				{
-					return null;
-				}
-				else
-				{
-					
-				}
-            })
-            .catch(err =>
-            {
-                console.log("error : "+err)
-            })
-			alert("Hello world");
-    }
-    constructor() 
-    {
-	    super()
-            this.state =
-            {
-				email: '',
-                password: ''
-            }
-	    this.onChange = this.onChange.bind(this);
-            this.onSubmit = this.onSubmit.bind(this);
-    }
+export default class PersonList extends React.Component {
+  state = {
+    name: '',
+  }
 
-    onChange(e)
-    {
-	    this.setState({[e.target.name]: e.target.value})
-    }
+  handleChange = event => {
+    this.setState({ name: event.target.value });
+  }
 
-    /*IsEmail(email)
-    {
-	    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            return re.test(String(email).toLowerCase());
-    }*/
+  handleSubmit = event => {
+    event.preventDefault();
 
-    onSubmit(e)
-    {
-        e.preventDefault();
-        const user =
-        {
-			email: this.state.email,
-            password: this.state.password
-        }
+    const user = {
+      name: this.state.name
+    };
+      alert(this.state.name);
 
-        if((user.email=="")&&(user.password==""))
-        {
-            window.Swal.fire
-            (
-                'Error',
-                'Email and password are empty',
-                'error'
-            )
-        }
-        else
-        {
-			if(user.email=="")
-            {
-                window.Swal.fire
-                (
-                    'Error',
-                    'Email is empty',
-                    'error'
-                )
-            }
-            else
-			{
-				this.insert(user).then
-				(
-					res =>
-					{
-						e.preventDefault();
-						if (res)
-						{
-							window.Swal.fire
-							(
-								'sucess!',
-								"insert good",
-								'sucess'
-							)
-						}
-						else
-						{
-							if(res==null)
-							{
-								window.Swal.fire
-								(
-									'Error!',
-									"Error",
-									'error'
-								)
-							}
-						}
-					}
-				)
-						
-			}
-        }
-    }
-    render() 
-	{
-        return (
-            <div id="back_ground">
-                <div className="container">
-                    <div className="row" id="rowLogin">
-                        <div className="col-md mt-8 mx-auto">
-                            <form
-                                className="measure " id="formLogin" noValidate onSubmit={this.onSubmit}>
-                                <center>
-                                    <h1 className="h3 mb-3 font-weight-normal">
-                                        Please sign in
-                                    </h1>
-                                </center>
-                                <div className='form pa4 br3 shadow-5'>
-                                    <div className="form-group">
-                                        <label htmlFor="email">
-                                            Value
-                                        </label>
-                                        <input type="email"
-                                               className="form-control"
-                                               name="email"
-                                               placeholder="Insert value"
-                                               value={this.state.email}
-                                               onChange={this.onChange}
-                                        />
-                                    </div>
-                                    
-                                    <button type="submit"
-                                            className="btn btn-lg btn-primary btn-block"
-                                    >
-                                        Add
-                                    </button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+    axios.post('http://127.0.0.1/zabbix/json/get_nb_value_sla.php', { user })
+      .then(res => {
+        console.log(res);
+        alert(res);
+        console.log(res.data);
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Person Name:
+            <input type="text" name="name" onChange={this.handleChange} />
+          </label>
+          <button type="submit">Add</button>
+        </form>
+      </div>
+    )
+  }
 }
-export default Register;
