95316daa7a9100b5aa24bf8433be7513f9282e57
Habib Aroua <HabibAroua@users.noreply.github.com>
2020-09-08 10:11:49 +0100

    Update Register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -2,38 +2,58 @@ import './Login.css';
 import React from 'react';
 import axios from 'axios';
 
-export default class PersonList extends React.Component {
-  state = {
-    name: '',
+export default class PersonList extends React.Component 
+{
+  state = 
+        {
+            name: '',
+        }
+
+  handleChange = event => 
+  {
+    this.setState
+    (
+        { 
+            name: event.target.value 
+        }
+    );
   }
 
-  handleChange = event => {
-    this.setState({ name: event.target.value });
-  }
-
-  handleSubmit = event => {
+  handleSubmit = event => 
+  {
     event.preventDefault();
 
-    const user = {
+    const user = 
+    {
       name: this.state.name
     };
 
-    axios.post('http://127.0.0.1/zabbix/json/get_nb_value_sla.php', {sla : this.state.name})
-      .then(res => {
-        console.log(res);
-        alert(res.data);
-        console.log(res.data);
-        window.Swal.fire
-        (
-            'Sucess',
-            res.data,
-            'success'
-        )
-      })
+    axios.post
+    (
+        'http://127.0.0.1/zabbix/json/get_nb_value_sla.php', 
+        {
+            sla : this.state.name
+        }
+    )
+    .then
+    (
+        res => 
+        {
+            console.log(res);
+            alert(res.data);
+            console.log(res.data);
+            window.Swal.fire
+            (
+                'Sucess',
+                res.data,
+                'success'
+            )
+        }
+    )
   }
-
-  render() {
-    return (
+  render() 
+  {
+      return (
       <div>
         <form onSubmit={this.handleSubmit}>
           <label>
