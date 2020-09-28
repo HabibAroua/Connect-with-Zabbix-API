45e5f12c33b0ec788f0938f8b927f90bd9def0db
Habib Aroua <habib.aroua@sesame.com.tn>
2020-09-28 23:23:22 +0100

    Update dataTables.bootstrap.js

--- htdocs/zabbix/web/Admin/assets/js/dataTables/dataTables.bootstrap.js
+++ htdocs/zabbix/web/Admin/assets/js/dataTables/dataTables.bootstrap.js
@@ -1,48 +1,67 @@
 /* Set the defaults for DataTables initialisation */
-$.extend(true, $.fn.dataTable.defaults, {
-    "sDom": "<'row'<'col-sm-6'l><'col-sm-6'f>r>" + "t" + "<'row'<'col-sm-6'i><'col-sm-6'p>>",
-    "oLanguage": {
-        "sLengthMenu": "_MENU_ records per page"
+$.extend
+(
+    true, $.fn.dataTable.defaults, 
+    {
+        "sDom": "<'row'<'col-sm-6'l><'col-sm-6'f>r>" + "t" + "<'row'<'col-sm-6'i><'col-sm-6'p>>",
+        "oLanguage": 
+        {
+            "sLengthMenu": "_MENU_ records per page"
+        }
     }
-});
+);
 
 
 /* Default class modification */
-$.extend($.fn.dataTableExt.oStdClasses, {
-    "sWrapper": "dataTables_wrapper form-inline",
-    "sFilterInput": "form-control input-sm",
-    "sLengthSelect": "form-control input-sm"
-});
+$.extend
+(
+    $.fn.dataTableExt.oStdClasses, 
+    {
+        "sWrapper": "dataTables_wrapper form-inline",
+        "sFilterInput": "form-control input-sm",
+        "sLengthSelect": "form-control input-sm"
+    }
+);
 
 // In 1.10 we use the pagination renderers to draw the Bootstrap paging,
 // rather than  custom plug-in
-if ($.fn.dataTable.Api) {
+if ($.fn.dataTable.Api) 
+{
     $.fn.dataTable.defaults.renderer = 'bootstrap';
-    $.fn.dataTable.ext.renderer.pageButton.bootstrap = function(settings, host, idx, buttons, page, pages) {
+    $.fn.dataTable.ext.renderer.pageButton.bootstrap = function(settings, host, idx, buttons, page, pages) 
+    {
         var api = new $.fn.dataTable.Api(settings);
         var classes = settings.oClasses;
         var lang = settings.oLanguage.oPaginate;
         var btnDisplay, btnClass;
 
-        var attach = function(container, buttons) {
+        var attach = function(container, buttons) 
+        {
             var i, ien, node, button;
-            var clickHandler = function(e) {
+            var clickHandler = function(e) 
+            {
                 e.preventDefault();
-                if (e.data.action !== 'ellipsis') {
+                if (e.data.action !== 'ellipsis') 
+                {
                     api.page(e.data.action).draw(false);
                 }
             };
 
-            for (i = 0, ien = buttons.length; i < ien; i++) {
+            for (i = 0, ien = buttons.length; i < ien; i++) 
+            {
                 button = buttons[i];
 
-                if ($.isArray(button)) {
+                if ($.isArray(button)) 
+                {
                     attach(container, button);
-                } else {
+                }
+                else
+                {
                     btnDisplay = '';
                     btnClass = '';
 
-                    switch (button) {
+                    switch (button) 
+                    {
                         case 'ellipsis':
                             btnDisplay = '&hellip;';
                             btnClass = 'disabled';
@@ -79,18 +98,30 @@ if ($.fn.dataTable.Api) {
                             break;
                     }
 
-                    if (btnDisplay) {
-                        node = $('<li>', {
-                            'class': classes.sPageButton + ' ' + btnClass,
-                            'aria-controls': settings.sTableId,
-                            'tabindex': settings.iTabIndex,
-                            'id': idx === 0 && typeof button === 'string' ? settings.sTableId + '_' + button : null
-                        })
-                            .append($('<a>', {
-                                    'href': '#'
-                                })
-                                .html(btnDisplay)
+                    if (btnDisplay)
+                    {
+                        node = 
+                            $
+                        (
+                            '<li>', 
+                             {
+                                'class': classes.sPageButton + ' ' + btnClass,
+                                'aria-controls': settings.sTableId,
+                                'tabindex': settings.iTabIndex,
+                                'id': idx === 0 && typeof button === 'string' ? settings.sTableId + '_' + button : null
+                            }
                         )
+                        .append
+                        (
+                            $
+                            (
+                                '<a>', 
+                                {
+                                    'href': '#'
+                                }
+                            )
+                            .html(btnDisplay)
+                         )
                             .appendTo(container);
 
                         settings.oApi._fnBindAction(
