7e9e6f73ea3fb7a9b3c38f17c77391aa0e7628ac
Habib Aroua <habib.aroua@sesame.com.tn>
2020-10-30 23:25:31 +0100

    Update action.php

--- htdocs/zabbix/web/app/controllers/action.php
+++ htdocs/zabbix/web/app/controllers/action.php
@@ -11,48 +11,47 @@
 		switch($action)
 		{
 			case 'delete_service' :	$serviceController = new serviceController();
-									if(isset($_POST['id']))
-									{
-										$test = $serviceController->delete($_POST['id']);
-										if($test)
-										{
-											echo "good";
-										}
-										else
-										{
-											echo "bad";
-										}	
-									}
-									else
-									{
-										echo "bad";
-									}
+						if(isset($_POST['id']))
+						{
+							$test = $serviceController->delete($_POST['id']);
+							if($test)
+							{
+								echo "good";
+							}
+							else
+							{
+								echo "bad";
+							}	
+						}
+						else
+						{
+							echo "bad";
+						}
 									
 			break;
 			case 'update_service' : $serviceController = new serviceController();
-									$host =new Host_Service();
+						$host =new Host_Service();
 									
-									if((isset($_POST['id'])) && (isset($_POST['host_name'])) && (isset($_POST['ip_address'])))
-									{
-										$serviceController = new serviceController();
-										$h = new Host_Service();
-										$h->setHost_name($_POST['host_name']);
-										$h->setIp_address($_POST['ip_address']);
-										$test = $serviceController->update($_POST['id'],$h);
-										if($test)
-										{
-											echo "good";
-										}
-										else
-										{
-											echo "bad";
-										}
-									}
-									else
-									{
-										echo "bad";
-									}
-									
+						if((isset($_POST['id'])) && (isset($_POST['host_name'])) && (isset($_POST['ip_address'])))
+						{
+							$serviceController = new serviceController();
+							$h = new Host_Service();
+							$h->setHost_name($_POST['host_name']);
+							$h->setIp_address($_POST['ip_address']);
+							$test = $serviceController->update($_POST['id'],$h);
+							if($test)
+							{
+								echo "good";
+							}
+							else
+							{
+								echo "bad";
+							}
+						}
+						else
+						{
+							echo "bad";
+						}			
 			break;
 		}
 	}
