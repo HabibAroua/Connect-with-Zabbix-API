8a6bc0e4c7478ad046339f59e5bd2bbe02302665
Habib Aroua <habib.aroua@sesame.com.tn>
2020-12-23 01:21:37 +0100

    Update logout.php

--- htdocs/zabbix/web/Admin/logout.php
+++ htdocs/zabbix/web/Admin/logout.php
@@ -1,8 +1,6 @@
 <?php
-
 	require_once('../app/controllers/session.php');
-    require_once('../app/controllers/AdminController.php');
+	require_once('../app/controllers/AdminController.php');
 	$s = new Session ();
-    $s->LogOut();
-	
+    	$s->LogOut();
 ?>
